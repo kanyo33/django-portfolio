cb136cce593a5a3c694c29913b86a54e270c94c9
test1 <n-k@live.ca>
2020-03-12 17:19:34 +0200

    first commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'tntfranzi33',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',
     }
